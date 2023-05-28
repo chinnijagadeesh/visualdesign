82b62d2e17eb1de563b8710d029c0350b99b6cc1
chinnijagadeesh <jagadeeshyadav5798@gmail.com>
2023-05-28 10:38:36 +0530

    visual-design

---
+++ src/components/Section.jsx
@@ -0,0 +1,255 @@
+import React from "react";
+import styles from "../components/Section.module.css";
+
+const Section = () => {
+  return (
+    <div className={styles.services}>
+      <div className={styles.relativeWrapperTwo}>
+        <div className={styles.line56} />
+        <div className={styles.line57} />
+        <p className={styles.servicesForEveryone}>
+          Services for everyone
+        </p>
+        <div className={styles.servicesForEveryoneTwo}>
+          <div className={styles.relativeWrapperOne}>
+            <div className={styles.speak1}>
+              <img
+                alt=""
+                className={styles.vector}
+                src="https://static.overlay-tech.com/assets/ce757f42-3965-4c39-83ad-9d7d78c62aaa.svg"
+              />
+            </div>
+            <div className={styles.rectangle22620} />
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Non native speakers
+            </p>
+          </div>
+        </div>
+        <div className={styles.servicesForEveryoneThree}>
+          <div className={styles.flexWrapperTwo}>
+
+            <div className={styles.boy11}>
+
+              <img
+                alt=""
+                className={styles.group}
+                src="https://static.overlay-tech.com/assets/115ea40f-6b69-4a04-9ce6-71bf706178e6.svg"
+              />
+            </div>
+            <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Unaccompanied Minors
+            </p>
+          </div>
+        </div>
+        <div className={styles.servicesForEveryoneFour}>
+          <div className={styles.flexWrapperTwo}>
+            <div className={styles.sunset1}>
+              <div className={styles.vectorTwo} />
+              <div className={styles.flexWrapperFour}>
+                
+                <img
+                  alt=""
+                  className={styles.vectorThree}
+                  src="https://static.overlay-tech.com/assets/42d4e278-c6b4-45e9-a7e2-94858d26f6fa.svg"
+                />
+                            <div className={styles.rectangle22621} />
+
+                <div className={styles.flexWrapperEleven}>
+                  <div className={styles.vectorFour} />
+                  <div className={styles.vectorFive} />
+                </div>
+                <div className={styles.vectorSix} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Leisure Travelers
+            </p>
+          </div>
+        </div>
+        <div className={styles.servicesForEveryoneFive}>
+          <div className={styles.flexWrapperTwo}>
+            <img
+              alt=""
+              className={styles.owner1}
+              src="https://static.overlay-tech.com/assets/780795d4-beff-4d30-b39c-0be1c8311aa9.svg"
+            />
+                        <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Travelling with <br />
+              Pets
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.relativeWrapperThree}>
+        <div className={styles.line56} />
+        <div className={styles.servicesForEveryoneTwo}>
+          <div className={styles.flexWrapperTwo}>
+            <div className={styles.businessman1}>
+              <img
+                alt=""
+                className={styles.vector}
+                src="https://static.overlay-tech.com/assets/404b67a9-0e5b-4a87-b2f2-6a781253fff7.svg"
+              />
+            </div>
+            <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Business Travellers, CXOs, HNIs
+            </p>
+          </div>
+        </div>
+        <div className={styles.servicesForEveryoneFour}>
+          <div className={styles.flexWrapperTwo}>
+            <img
+              alt=""
+              className={styles.owner1}
+              src='celebrity.png'
+              // src="https://static.overlay-tech.com/assets/da4f25ac-9064-4729-b129-b39b68e8091a.png"
+            />
+          </div>
+          <div className={styles.rectangle22621} />
+
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Celebrities &amp; Public Figures
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.relativeWrapperFour}>
+        <div className={styles.line59} />
+        <div className={styles.servicesForEveryoneSix}>
+          <div className={styles.flexWrapperTwo}>
+            <div className={styles.family21}>
+              <img
+                alt=""
+                className={styles.vector}
+                src="https://static.overlay-tech.com/assets/39994a1b-c292-4bc4-82b3-2565c81d2ee1.svg"
+              />
+            </div>
+            <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.families}>Families</p>
+          </div>
+        </div>
+        <div className={styles.servicesForEveryoneFour}>
+          <div className={styles.flexWrapperTwo}>
+            <img
+              alt=""
+              className={styles.owner1}
+              src="https://static.overlay-tech.com/assets/83fd4e35-8410-4182-8ccb-0e3b6a8208f8.svg"
+            />
+                        <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Group or Corporate Travellers
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.relativeWrapperFive}>
+        <div className={styles.line56} />
+        <div className={styles.servicesForEveryoneTwo}>
+          <div className={styles.flexWrapperTwo}>
+            <div className={styles.g2589}>
+              <img
+                alt=""
+                className={styles.vector}
+                src="oldman.png"
+
+                // src="https://static.overlay-tech.com/assets/906c0882-03a5-4834-a16a-5f4a5f199dd5.svg"
+              />
+
+            </div>
+            <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Senior <br />
+              Citizens
+            </p>
+          </div>
+        </div>
+        <div className={styles.servicesForEveryoneFour}>
+          <div className={styles.flexWrapperTwo}>
+            <img
+              alt=""
+              className={styles.owner1}
+              src='wheelchair.png'
+              // src="https://static.overlay-tech.com/assets/f99a2b1b-259a-4eee-9712-b04a7da124c7.png"
+            />
+                        <div className={styles.rectangle22621} />
+
+          </div>
+          <div className={styles.frame3393}>
+            <div className={styles.frame3394}>
+              <div className={styles.rectangle55} />
+              <div className={styles.rectangle56} />
+            </div>
+            <p className={styles.nonNativespeakers}>
+              Special <br />
+              Needs
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section
